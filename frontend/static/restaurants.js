4543bc4d90d8264e3142abf005d815e6d3cd88f1
Yuvraj Bhatia <bhatiayuvraj58@gmail.com>
2024-12-07 00:09:26 -0700

    Remove frontend folder

--- frontend/static/restaurants.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const logoutButton = document.getElementById("logoutButton");
-
-    if (logoutButton) {
-        logoutButton.addEventListener("click", () => {
-            window.location.href = "/logout"; 
-        });
-    }
-});
